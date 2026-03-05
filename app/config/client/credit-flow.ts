4952d0f0545018070b1a99a2707bff39ca77a8aa
cerciber <catorres@fgs.co>
2026-03-04 22:04:16 -0500

    feat: app shell complete flow

--- app/config/client/credit-flow.ts
+++ app/config/client/credit-flow.ts
@@ -1,4 +1,6 @@
 export const CREDIT_FLOW_URLS: string[] = [
   'http://localhost:3003/offer?nav=false',
-  'http://localhost:3003/offer?nav=false',
+  'http://localhost:3004/account?nav=false',
+  'http://localhost:3005/beneficiary?nav=false',
+  'http://localhost:3006/legalization?nav=false',
 ];
